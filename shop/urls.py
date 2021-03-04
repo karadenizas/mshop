ef9d61592bb9b43c78e919e7b6251cd9a0761683
salih <asalihkaradeniz@gmail.com>
2021-03-04 20:55:25 +0300

    pagination added

--- shop/urls.py
+++ shop/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
+    path('test/', views.test, name='test'),
     path('', views.index, name='index'),
     path('<slug:category_slug>/', views.index, name='category_list'),
     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
--- shop/views.py
+++ shop/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def index(request, category_slug=None):
     category = None
     products = None
+    paginate_products = None
 
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=category)
+    
+        paginator = Paginator(products, 6)
+        page = request.GET.get('page')
+        try:
+            paginate_products = paginator.page(page)
+        except PageNotAnInteger:
+            paginate_products = paginator.page(1)
+        except EmptyPage:
+            paginate_products = paginator.page(paginator.num_pages)
 
     context = {
         'category': category,
         'products': products,
+        'paginate_products': paginate_products,
     }
 
     return render(request, 'shop/index.html', context)
@@ -25,3 +37,7 @@ def product_detail(request, id, slug):
         'product': product,
     }
     return render(request, 'shop/detail.html', context)
+
+
+def test(request):
+    return render(request, 'test.html')
