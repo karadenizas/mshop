f2ac86f95813a99494857e52f779afcd6b9bfe42
karadenizas <asalihkaradeniz@gmail.com>
2021-09-22 11:36:25 +0300

    test

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
-app_name = 'account'
 
 urlpatterns = [
     path('', views.account, name='account'),
+    path('', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
 ]
